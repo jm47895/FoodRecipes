df0309c4bdec61dfef6a7e0a11f98afae4acf229
jm47895 <jordan.madrigal@gmail.com>
2020-03-14 23:25:09 -0500

    Build out GET for recipe API client methods

--- app/src/main/java/com/jordanmadrigal/foodrecipes/requests/RecipeApi.java
+++ app/src/main/java/com/jordanmadrigal/foodrecipes/requests/RecipeApi.java
@@ -1,4 +1,27 @@
 package com.jordanmadrigal.foodrecipes.requests;
 
+import com.jordanmadrigal.foodrecipes.requests.responses.RecipeResponse;
+import com.jordanmadrigal.foodrecipes.requests.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RecipeApi {
+
+    //search
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
+
+    //individual recipe
+    @GET("api/get")
+    Call<RecipeResponse> getRecipe(
+            @Query("key") String key,
+            @Query("rId") String recipe_id
+    );
+
 }
--- app/src/main/java/com/jordanmadrigal/foodrecipes/utils/Constants.java
+++ app/src/main/java/com/jordanmadrigal/foodrecipes/utils/Constants.java
@@ -3,5 +3,6 @@ package com.jordanmadrigal.foodrecipes.utils;
 public class Constants {
 
     public static final String BASE_URL = "https://recipesapi.herokuapp.com";
+    public static final String API_KEY = "";
 
 }
