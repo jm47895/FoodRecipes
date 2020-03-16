82413772b459c9afce0c67b860b9fb3cd45cccb4
jm47895 <jordan.madrigal@gmail.com>
2020-03-16 13:41:46 -0500

    Add runnable to Recipe Api Client

--- app/src/main/java/com/jordanmadrigal/foodrecipes/requests/responses/RecipeApiClient.java
+++ app/src/main/java/com/jordanmadrigal/foodrecipes/requests/responses/RecipeApiClient.java
@@ -2,20 +2,31 @@ package com.jordanmadrigal.foodrecipes.requests.responses;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.util.Log;
 
 import com.jordanmadrigal.foodrecipes.AppExecutors;
 import com.jordanmadrigal.foodrecipes.models.Recipe;
+import com.jordanmadrigal.foodrecipes.requests.RecipeApi;
+import com.jordanmadrigal.foodrecipes.requests.ServiceGenerator;
+import com.jordanmadrigal.foodrecipes.utils.Constants;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 import static com.jordanmadrigal.foodrecipes.utils.Constants.NETWORK_TIMEOUT;
 
 public class RecipeApiClient {
 
+    private static final String TAG = "RecipeApiClient";
     private static RecipeApiClient instance;
     private MutableLiveData<List<Recipe>> recipes;
+    private RetrieveRecipesRunnable retrieveRecipesRunnable;
 
     public static RecipeApiClient getInstance(){
         if(instance == null){
@@ -33,8 +44,13 @@ public class RecipeApiClient {
         return recipes;
     }
 
-    public void searchRecipesApi(){
-        final Future handler = AppExecutors.getInstance().getNetworkIO().submit();
+    public void searchRecipesApi(String query, int pageNumber){
+        if(retrieveRecipesRunnable != null){
+            retrieveRecipesRunnable = null;
+        }
+
+        retrieveRecipesRunnable = new RetrieveRecipesRunnable(query, pageNumber);
+        final Future handler = AppExecutors.getInstance().getNetworkIO().submit(retrieveRecipesRunnable);
 
         AppExecutors.getInstance().getNetworkIO().schedule(new Runnable() {
             @Override
@@ -43,5 +59,56 @@ public class RecipeApiClient {
             }
         }, NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
     }
+
+    private class RetrieveRecipesRunnable implements Runnable{
+
+        private String query;
+        private int pageNumber;
+        private boolean cancelRequest;
+
+        private RetrieveRecipesRunnable(String query, int pageNumber) {
+            this.query = query;
+            this.pageNumber = pageNumber;
+            cancelRequest = false;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Response response = getRecipes(query, pageNumber).execute();
+                if(cancelRequest){
+                    return;
+                }
+
+                if(response.code() == 200){
+                    List<Recipe> list = new ArrayList<>(((RecipeSearchResponse)response.body()).getRecipes());
+                    if(pageNumber == 1){
+                        recipes.postValue(list);
+                    }else{
+                        List<Recipe> currentRecipes = recipes.getValue();
+                        currentRecipes.addAll(list);
+                        recipes.postValue(currentRecipes);
+                    }
+                }else{
+                    String error = response.errorBody().string();
+                    Log.e(TAG, "Response Error: " + error);
+                    recipes.postValue(null);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                recipes.postValue(null);
+            }
+        }
+
+        private Call<RecipeSearchResponse> getRecipes(String query, int pageNumber){
+            return ServiceGenerator.getRecipeApi().searchRecipe(Constants.API_KEY, query, String.valueOf(pageNumber));
+        }
+
+        private void cancelRequest(){
+            Log.d(TAG, "Cancelling search query");
+            cancelRequest = true;
+        }
+    }
+
 
 }
